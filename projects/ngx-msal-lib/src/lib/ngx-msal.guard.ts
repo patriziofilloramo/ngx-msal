3848a560907f520734bf82a831397166294b8ea7
Patrizio Filloramo <patrizio.filloramo@unibz.it>
2020-03-28 15:20:49 +0100

    Update redirect handling

--- projects/ngx-msal-lib/src/lib/ngx-msal.guard.ts
+++ projects/ngx-msal-lib/src/lib/ngx-msal.guard.ts
@@ -1,9 +1,11 @@
-import { Inject, Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
+import { Inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { AuthenticationParameters, AuthError, AuthResponse } from 'msal';
 
 import { MsalConfiguration, NGX_MSAL_CONFIG, NgxMsalConfig } from './ngx-msal.config';
 import { MsalService } from './ngx-msal.service';
+import { WindowUtils } from 'msal/lib-commonjs/utils/WindowUtils';
+import { UrlUtils } from 'msal/lib-commonjs/utils/UrlUtils';
 
 @Injectable({
     providedIn: 'root'
@@ -12,9 +14,9 @@ export class MsalGuard implements CanActivate {
     private _msalConfig: MsalConfiguration;
     private _requestAuthParams: AuthenticationParameters;
 
-    constructor( 
+    constructor(
         private _msalSvc: MsalService,
-        @Inject(NGX_MSAL_CONFIG) private msalConfig: NgxMsalConfig) 
+        @Inject(NGX_MSAL_CONFIG) private msalConfig: NgxMsalConfig)
     {
         this._msalConfig = this.msalConfig.config;
         this._requestAuthParams = {
@@ -23,22 +25,26 @@ export class MsalGuard implements CanActivate {
         };
     }
 
-    
+
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Promise<boolean> {
+
+        // If a page with MSAL Guard is set as the redirect for acquireTokenSilent,
+        // short-circuit to prevent redirecting or popups.
+        if (UrlUtils.urlContainsHash(window.location.hash) && WindowUtils.isInIframe()) {
+            return false;
+        }
 
         if (!this._msalSvc.getAccount() ) {
                 if (this._msalConfig.framework.popUp) {
                     return this._msalSvc.loginPopup(this._requestAuthParams)
                         .then(() => true)
                         .catch(() => false);
-                }
-                else {
+                } else {
                     this._msalSvc.loginRedirect(this._requestAuthParams);
                 }
-            }
-        else {
-            return this._msalSvc.acquireTokenSilent({ 
-                scopes: [this._msalConfig.auth.clientId], 
+            } else {
+            return this._msalSvc.acquireTokenSilent({
+                scopes: [this._msalConfig.auth.clientId],
                 authority: this._msalConfig.auth.authority
             })
             .then((result: AuthResponse) => {
@@ -46,17 +52,17 @@ export class MsalGuard implements CanActivate {
                 })
                 .catch((error: AuthError) => {
                     /**
-                     * "block_token_requests" error code can be ignored. 
+                     * "block_token_requests" error code can be ignored.
                      * Msal is blocking calls when they are fired from an iframe
-                     * */
+                     */
                     if (error && error.errorCode && error.errorCode !== 'block_token_requests') {
-                        console.error('MsalGuard - Acquire token silent error. Error code:', error.errorCode);                  
+                        console.error('MsalGuard - Acquire token silent error. Error code:', error.errorCode);
                     }
 
                     /* workaround: multiple instance running together at the same time may have create
                     duplicated localstorage token entries with the same values. We can ignore it. */
-                    if (error.errorCode === 'multiple_matching_tokens') return true;
-                    
+                    if (error.errorCode === 'multiple_matching_tokens') { return true; }
+
                     return false;
                 });
         }
