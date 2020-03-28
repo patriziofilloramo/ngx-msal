693ef14787ca45be973319ea0da1767c89b3bfb1
Patrizio Filloramo <patrizio.filloramo@unibz.it>
2020-03-28 13:28:32 +0100

    Add interceptor provider and clean code for tslint

--- projects/ngx-msal-showcase/src/app/app.module.ts
+++ projects/ngx-msal-showcase/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MsalConfiguration, MsalModule } from 'ngx-msal-lib';
+import { MsalConfiguration, MsalModule, MsalInterceptor } from 'ngx-msal-lib';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,31 +9,27 @@ import { MainComponent } from './components/main/main.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 const isIE =
-  window.navigator.userAgent.indexOf("MSIE ") > -1 ||
-  window.navigator.userAgent.indexOf("Trident/") > -1;
+  window.navigator.userAgent.indexOf('MSIE ') > -1 ||
+  window.navigator.userAgent.indexOf('Trident/') > -1;
 
 export const msalConfig: MsalConfiguration = {
   auth: {
-    // clientId: "YOUR_AZUREAPP_CLIENT_ID_HERE",
-    // authority:
-    //   "https://login.microsoftonline.com/XXXXXXXXXXX",
-    clientId: "aaf3f968-94f2-454f-9d58-aa4571e1c880",
-    authority:
-      "https://login.microsoftonline.com/scientificnet.onmicrosoft.com/",
+    clientId: 'YOUR_AZUREAPP_CLIENT_ID_HERE',
+    authority: 'https://login.microsoftonline.com/XXXXXXXXXXX',
     validateAuthority: true,
     // redirectUri: 'https://localhost:4200/#/redirect',
     navigateToLoginRequestUrl: true
   },
   cache: {
-    cacheLocation: "localStorage" as any,
+    cacheLocation: 'localStorage' as any,
     storeAuthStateInCookie: true
   },
   framework: {
     popUp: false,
-    protectedResourceMap: [["https://graph.microsoft.com", ["User.Read"]]],
+    protectedResourceMap: [['https://graph.microsoft.com', ['User.Read']]],
     unprotectedResources: [],
-    consentScopes: ["User.Read"],
-    
+    consentScopes: ['User.Read'],
+
   }
 };
 
@@ -53,6 +49,11 @@ export const msalConfig: MsalConfiguration = {
     HttpClientModule
   ],
   providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
